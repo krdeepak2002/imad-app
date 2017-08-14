2bde9b341852021827ea5e7ddae0c9a4a639d50e
krdeepak2002 <30894137+krdeepak2002@users.noreply.github.com>
2017-08-14 20:59:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-on',function(rea,res){
+app.get('/article-one',function(rea,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     
 });
